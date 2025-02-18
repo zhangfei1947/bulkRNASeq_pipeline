9e5233629992f54c327a49dd02d1d64205ed14c0
zhangfei1947 <zhang_fei_2008@yeah.net>
2025-02-18 11:04:54 -0600

    runsnk

--- scripts/pca.py
+++ scripts/pca.py
@@ -33,40 +33,33 @@ def main():
     # Extracting metadata for coloring
     pca_df['Tissue'] = pca_df['Sample'].str.split("_").str[2].str[0]
     pca_df['Day'] = pca_df['Sample'].str.split("_").str[0]
-    pca_df['Treatment'] = pca_df['Sample'].str.split("_").str[1]
+    #pca_df['Treatment'] = pca_df['Sample'].str.split("_").str[1]
 
     # Plotting
-    n = 5
-    fig, axes = plt.subplots(1, n, figsize=(n*(4.2), 4))
+    fig, axes = plt.subplots(1, 3, figsize=(3*4.2, 4))
 
     sns.scatterplot(x='PC1', y='PC2', hue='Treatment', data=pca_df, ax=axes[2], s=100)
-    axes[2].set_title('PCA Colored by Treatment')
+    #axes[0].set_title('PCA Colored by Treatment')
+    axes[0].set_xlabel(f'PC1 ({pca.explained_variance_ratio_[0]*100:.2f}%)') #add explained variance ratio
+    axes[0].set_ylabel(f'PC2 ({pca.explained_variance_ratio_[1]*100:.2f}%)') #add explained variance ratio
+    axes[0].set_aspect('auto', adjustable='box') # Make subplots square
 
     sns.scatterplot(x='PC2', y='PC3', hue='Treatment', data=pca_df, ax=axes[3], s=100)
-    axes[3].set_title('PCA Colored by Treatment')
+    #axes[1].set_title('PCA Colored by Treatment')
+    axes[1].set_xlabel(f'PC2 ({pca.explained_variance_ratio_[1]*100:.2f}%)') #add explained variance ratio
+    axes[1].set_ylabel(f'PC3 ({pca.explained_variance_ratio_[2]*100:.2f}%)') #add explained variance ratio
+    axes[1].set_aspect('auto', adjustable='box') # Make subplots square
 
     sns.scatterplot(x='PC1', y='PC3', hue='Treatment', data=pca_df, ax=axes[4], s=100)
-    axes[4].set_title('PCA Colored by Treatment')
-
-    for ax in axes[:3]:
-        ax.set_xlabel(f'PC1 ({pca.explained_variance_ratio_[0]*100:.2f}%)') #add explained variance ratio
-        ax.set_ylabel(f'PC2 ({pca.explained_variance_ratio_[1]*100:.2f}%)') #add explained variance ratio
-        ax.set_aspect('auto', adjustable='box') # Make subplots square
-
-'''
-    axes[3].set_xlabel(f'PC2 ({pca.explained_variance_ratio_[1]*100:.2f}%)') #add explained variance ratio
-    axes[3].set_ylabel(f'PC3 ({pca.explained_variance_ratio_[2]*100:.2f}%)') #add explained variance ratio
-    axes[3].set_aspect('auto', adjustable='box') # Make subplots square
-
-    axes[4].set_xlabel(f'PC1 ({pca.explained_variance_ratio_[0]*100:.2f}%)') #add explained variance ratio
-    axes[4].set_ylabel(f'PC3 ({pca.explained_variance_ratio_[2]*100:.2f}%)') #add explained variance ratio
-    axes[4].set_aspect('auto', adjustable='box') # Make subplots square
-'''
+    #axes[2].set_title('PCA Colored by Treatment')
+    axes[2].set_xlabel(f'PC1 ({pca.explained_variance_ratio_[1]*100:.2f}%)') #add explained variance ratio
+    axes[2].set_ylabel(f'PC3 ({pca.explained_variance_ratio_[2]*100:.2f}%)') #add explained variance ratio
+    axes[2].set_aspect('auto', adjustable='box') # Make subplots square
 
     plt.tight_layout()
     plt.savefig(snakemake.output[0])
     plt.show()
-
+    
 if __name__ == "__main__":
     main()
 
