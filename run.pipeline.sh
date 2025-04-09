954a78a7f35456aa3ac06245f52357a01924ae81
zhangfei1947 <zhang_fei_2008@yeah.net>
2025-04-09 15:25:33 -0500

    conda

--- run.pipeline.sh
+++ run.pipeline.sh
@@ -8,5 +8,5 @@ export R_LIBS_USER="/scratch/group/lilab/software/R_library/4.3"
 pipeline_dir=/scratch/group/lilab/Phil/bulkRNASeq_pipeline
 
 
-snakemake --snakefile ${pipeline_dir}/Snakefile --configfile config.yaml 
+snakemake --snakefile ${pipeline_dir}/workflow/Snakefile --configfile config.yaml 
 
