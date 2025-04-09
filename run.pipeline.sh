e945816d12f59889dd730d55da5d40150683e860
zhangfei1947 <zhang_fei_2008@yeah.net>
2025-04-09 16:45:23 -0500

    docker

--- run.pipeline.sh
+++ run.pipeline.sh
@@ -6,6 +6,8 @@ source activate snakemake732
 export R_LIBS_USER="/scratch/group/lilab/software/R_library/4.4"
 export PATH="/scratch/group/lilab/software/jq:$PATH"
 export PATH="/scratch/group/lilab/software/subread-2.1.0/bin:$PATH"
+export PATH="/scratch/group/lilab/software/hisat2-2.2.1:$PATH"
+
 
 pipeline_dir=/scratch/group/lilab/Phil/bulkRNASeq_pipeline
 snakemake \
