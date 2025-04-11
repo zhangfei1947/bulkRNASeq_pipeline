7853361ba4c155b17f35c01f752c382656945579
zhangfei1947 <zhang_fei_2008@yeah.net>
2025-04-11 13:51:57 -0500

    cluster

--- run.pipeline.sh
+++ run.pipeline.sh
@@ -3,12 +3,12 @@
 module load Anaconda3/2024.02-1
 source activate snakemake732
 
-export R_LIBS_USER="/scratch/group/lilab/software/R_library/4.3"
+export R_LIBS_USER="/scratch/group/lilab/software/R_library/4.4"
 export PATH="/scratch/group/lilab/software/jq:$PATH"
 export PATH="/scratch/group/lilab/software/subread-2.1.0/bin:$PATH"
 export PATH="/scratch/group/lilab/software/hisat2-2.2.1:$PATH"
 
-
+dockerfile=/scratch/user/philzhang/RNASeq_container.sif
 pipeline_dir=/scratch/group/lilab/Phil/bulkRNASeq_pipeline
 snakemake \
  --snakefile ${pipeline_dir}/workflow/Snakefile \
