01a7d0b84cd2fcc2403e36f70b8f4e8437f0f156
zhangfei1947 <zhang_fei_2008@yeah.net>
2025-04-11 13:41:56 -0500

    cluster

--- run.pipeline.sh
+++ run.pipeline.sh
@@ -3,7 +3,7 @@
 module load Anaconda3/2024.02-1
 source activate snakemake732
 
-export R_LIBS_USER="/scratch/group/lilab/software/R_library/4.4"
+export R_LIBS_USER="/scratch/group/lilab/software/R_library/4.3"
 export PATH="/scratch/group/lilab/software/jq:$PATH"
 export PATH="/scratch/group/lilab/software/subread-2.1.0/bin:$PATH"
 export PATH="/scratch/group/lilab/software/hisat2-2.2.1:$PATH"
