26937e554d922d094f639a36914620867b571ee7
zhangfei1947 <zhang_fei_2008@yeah.net>
2025-05-15 16:14:20 -0500

    corr

--- workflow/scripts/corr.py
+++ workflow/scripts/corr.py
@@ -6,16 +6,14 @@ import sys, re
 
 def main():
     filepath = snakemake.input.norm_counts
-    print(filepath)
     outfile = snakemake.output
     target_groups = snakemake.params.target_groups
     sample_mapping = snakemake.params.sample_mapping
-    print(target_groups)
-    print(sample_mapping)
 
     df = pd.read_csv(filepath, sep='\t', index_col=0)
     df_log2 = np.log2(df + 0.01)
     valid_samples = [s for s in df_log2.columns if sample_mapping.get(s.split(".")[0]) in target_groups]
+    print(valid_samples)
 
     group_df = df_log2[valid_samples]
     correlation_matrix = group_df.corr(method='pearson')
